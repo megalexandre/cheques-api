21a23bde88b3d5cafb0f47f89b4a346a8cb0827b
Alexandre Bezerra de Queiroz <alexandre.queiroz@syngenta.com>
2026-01-11 08:09:04 -0300

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://calculacheque.online/cheques', 
+  apiUrl: 'https://calculacheque.online', 
   defaultauth: 'fackbackend',
   firebaseConfig: {
     apiKey: '',
